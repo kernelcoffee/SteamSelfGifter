541f764de8262e26ef784e6de3ae9d457121b5cb
Alexandre Moore <alexandre.j.moore@gmail.com>
2020-02-18 23:21:39 -0800

    Update settings file to actuallement take the config file into account

--- steamselfgifter/settings.py
+++ steamselfgifter/settings.py
@@ -6,24 +6,24 @@ import os
 import random
 
 logger = logging.getLogger(__name__)
-loggin_format="%(asctime)s %(levelname)s-%(filename)s::%(funcName)s::%(lineno)d - %(message)s"
+logging_format="%(asctime)s %(levelname)s %(filename)s::%(funcName)s::%(lineno)d - %(message)s"
 
 config = configparser.ConfigParser()
 
 MAIN_URL = "https://www.steamgifts.com"
 WISHLIST_URL = "https://www.steamgifts.com/giveaways/search?"
 
-upper_threshold = 350
-lower_threshold = 200
-game_min_price = 10
-game_min_score = 7
-game_min_reviews = 500
 
 def init():
     global cookie
     global headers
     global xsrf_token
     global points
+    global upper_threshold
+    global lower_threshold
+    global game_min_price
+    global game_min_score
+    global game_min_reviews
 
     # Init options
     parser = argparse.ArgumentParser()
@@ -38,23 +38,28 @@ def init():
         config.read(config_path)
         session_id = config["cookies"]["PHPSESSID"]
         log_level = config["misc"]["logging"]
-        upper_threshold = config["misc"]["upper_threshold"]
-        lower_threshold = config["misc"]["lower_threshold"]
-        game_min_price = config["game"]["min_price"]
-        game_min_score = config["game"]["min_score"]
-        game_min_reviews = config["game"]["min_reviews"]
-
+        upper_threshold = int(config["misc"]["upper_threshold"])
+        lower_threshold = int(config["misc"]["lower_threshold"])
+        game_min_price = int(config["game"]["min_price"])
+        game_min_score = int(config["game"]["min_score"])
+        game_min_reviews = int(config["game"]["min_reviews"])
     else:
-        log_level = os.environ.get("LOGGING")
         session_id = os.environ.get("PHPSESSID")
+        log_level = os.environ.get("LOGGING")
+        upper_threshold = 350
+        lower_threshold = 200
+        game_min_price = 10
+        game_min_score = 7
+        game_min_reviews = 500
+
 
     # VERBOSE
     if args.verbose or log_level == "INFO":
-        logging.basicConfig(level=logging.INFO, format=loggin_format)
+        logging.basicConfig(level=logging.INFO, format=logging_format)
 
     # DEBUG
     if args.debug or log_level == "DEBUG":
-        logging.basicConfig(level=logging.DEBUG, format=loggin_format)
+        logging.basicConfig(level=logging.DEBUG, format=logging_format)
 
     user_agent = "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0"
 
