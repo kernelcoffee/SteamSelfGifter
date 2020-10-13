1dd6b6feed522bfe354a335d00d378b97977e83f
Alexandre Moore <alexandre.j.moore@gmail.com>
2020-10-13 10:54:52 -0700

    Fix missing attribute

--- steamselfgifter/steam/steamgame.py
+++ steamselfgifter/steam/steamgame.py
@@ -9,6 +9,7 @@ logger = logging.getLogger(__name__)
 class SteamGame:
     def __init__(self, steamid):
         self.steamid = steamid
+        self.modified_at = datetime.datetime.utcnow()
 
     def _update_data(self):
         data = ""
